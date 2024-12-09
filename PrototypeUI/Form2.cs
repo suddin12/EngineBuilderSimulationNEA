bd26ce7b738c4c879281935f166827c169860a33
Saif <100427056+suddin12@users.noreply.github.com>
2024-12-09 21:51:34 +0000

    Add files via upload

---
+++ PrototypeUI/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype_UI
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            Form f3 = new Form3();
+            f3.Show();
+        }
+    }
+}
